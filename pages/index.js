a7225bb14a9255837df9bdd6b08828e14ffab662
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-01-24 14:59:57 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,43 @@
+function GlobalStyle() {
+  return (
+    <style global jsx>{`
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
+
+      body {
+        font-family: sans-serif;
+      }
+    `}</style>
+  );
+}
+
+function Title(props) {
+  const Tag = props.tag;
+  return (
+    <>
+      <Tag>{props.children}</Tag>
+      <style jsx>{`
+        ${Tag} {
+          color: red;
+          font-size: 24px;
+          font-weight: 600;
+        }
+      `}</style>
+    </>
+  );
+}
+
+function HomePage() {
+  return (
+    <div>
+      <GlobalStyle />
+      <Title tag="h2">Boas Vindas de Volta!</Title>
+      <h2>Discord - Alura Matrix</h2>
+    </div>
+  );
+}
+
+export default HomePage;
